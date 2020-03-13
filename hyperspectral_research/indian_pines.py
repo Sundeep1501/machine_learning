2adc06e7777987014085dd6d6bf60412cafa12bc
Sundeep Penkay <sundeep1501@Sundeeps-MacBook-Pro.local>
2020-03-13 17:25:45 -0500

    Added new conv1d layer and saved the model. trained the model 3 times. no weights have been transferred. network is same and random

--- hyperspectral_research/indian_pines.py
+++ hyperspectral_research/indian_pines.py
@@ -17,6 +17,7 @@ print(gt.shape)
 
 classes = np.zeros(17)
 
+# 145*145 = 21025
 for i in range(145):
     for j in range(145):
         classes[gt[i][j]] = classes[gt[i][j]] + 1
@@ -24,18 +25,63 @@ for i in range(145):
 print(classes)
 
 # READ PIXEL DATA ALL BANDS
-X = np.empty((10249, 200))
+# 21025 - 10776 - 46 -28 -20 -93
+X = np.empty((10062, 200))
 Y = np.array([])
 
 index = 0
 
 for i in range(145):
     for j in range(145):
-        # remove background pixels
-        if gt[i][j] != 0:
-            X[index] = np.array(data[i][j])
-            Y = np.append(Y, gt[i][j]-1)
-            index = index+1
+        # remove background pixels and low samples records
+        # 0,1,7,9,16
+        cat = gt[i][j]
+        if cat == 0 or cat == 1 or cat == 7 or cat == 9 or cat == 16:
+            # skip
+            continue
+
+        nCat = cat
+        if cat == 2:
+            # 1
+            nCat = 0
+        elif cat == 3:
+            # 2
+            nCat = 1
+        elif cat == 4:
+            # 3
+            nCat = 2
+        elif cat == 5:
+            # 4
+            nCat = 3
+        elif cat == 6:
+            # 5
+            nCat = 4
+        elif cat == 8:
+            # 6
+            nCat = 5
+        elif cat == 10:
+            # 7
+            nCat = 6
+        elif cat == 11:
+            # 8
+            nCat = 7
+        elif cat == 12:
+            # 9
+            nCat = 8
+        elif cat == 13:
+            # 10
+            nCat = 9
+        elif cat == 14:
+            # 11
+            nCat = 10
+        elif cat == 15:
+            # 12
+            nCat = 11
+        
+        X[index] = np.array(data[i][j])
+        index = index+1
+        Y = np.append(Y, nCat)
+
 
 Y = to_categorical(Y)
 print(X.shape)
@@ -66,19 +112,19 @@ import keras.backend as K
 
 model = Sequential()
 
-salinas_model = load_model('salinas.h5')
-weights = salinas_model.get_layer(index=0).get_weights()
-weights = np.array(weights)
-weights = weights[0]
-weights = np.transpose(weights)
-print(weights.shape)
 # Add Conv1D layer which takes band vector of each pixel
 model.add(Conv1D(filters=64,
                 kernel_size=10,
                 input_shape=(X_train.shape[1],1),
-                activation='relu',
-                # kernel_initializer = keras.initializers.constant(weights),
-                # trainable=False
+                activation='relu'
+                ))
+
+# Add MaxPooling layer to decrese the size of the feature maps
+model.add(MaxPooling1D(pool_size = 2))
+
+model.add(Conv1D(filters=64,
+                kernel_size=10,
+                activation='relu'
                 ))
 
 # Add MaxPooling layer to decrese the size of the feature maps
@@ -95,25 +141,23 @@ model.add(Dense(Y_train[0].shape[0], activation='softmax'))
 
 # Compile
 model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adam(),
+              optimizer=keras.optimizers.Adagrad(),
               metrics=['accuracy'])
 
 model.fit(X_train, Y_train,
             batch_size=32,
             epochs=200)
 
-model.save('indian_pines_2.h5')
-
 
 y_pred = model.predict_classes(X_test)
 
-target_labels = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"]
+target_labels = ["A","B","C","D","E","F","G","H","I","J","K","L"]
 
 from sklearn.metrics import classification_report, confusion_matrix
-print(classification_report(np.argmax(Y_test,axis=1),y_pred,target_names=target_labels))
-cm = confusion_matrix(np.argmax(Y_test,axis=1),y_pred)
+print(classification_report(np.argmax(Y_test, axis=1), y_pred, target_names=target_labels))
+cm = confusion_matrix(np.argmax(Y_test, axis=1), y_pred)
 print(cm)
-
+model.save('indian_pines_5.h5')
 
 # model = load_model('my_model.h5')
 
--- hyperspectral_research/read_weights.py
+++ hyperspectral_research/read_weights.py
@@ -2,13 +2,13 @@ import numpy as np
 import keras
 from keras.models import load_model
 
-model = load_model('salinas.h5')
+model = load_model('indian_pines_5.h5')
 weights = model.get_layer(index=0).get_weights()
 weights = np.array(weights)
 weights = weights[0]
 weights = np.transpose(weights)
 print(weights.shape)
 print(weights)
-with open('salinas.txt', 'w') as outfile:
+with open('indian_pines_5.txt', 'w') as outfile:
     for slice_2d in weights:
         np.savetxt(outfile, slice_2d)
